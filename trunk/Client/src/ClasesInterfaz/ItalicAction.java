b250c5a38af3d8957e13550ed7e8acd2616a23d1
alejo@masfm.com <alejo@masfm.com@d7aedbdb-e078-e6c9-b6fc-c904269d7ce7>
2012-11-17 03:43:02 +0000

    para que funcione alta

---
+++ trunk/Client/src/ClasesInterfaz/ItalicAction.java
@@ -0,0 +1,31 @@
+package ClasesInterfaz;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JEditorPane;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledEditorKit;
+
+
+public class ItalicAction extends StyledEditorKit.StyledTextAction {
+    public ItalicAction() {
+        super("font-italic");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+            JEditorPane editor = getEditor(e);
+            if (editor != null) {
+                StyledEditorKit kit = getStyledEditorKit(editor);
+                MutableAttributeSet attr = kit.getInputAttributes();
+                boolean italic = (StyleConstants.isItalic(attr)) ? false : true;
+                SimpleAttributeSet sas = new SimpleAttributeSet();
+                StyleConstants.setItalic(sas, italic);
+                setCharacterAttributes(editor, sas, false);
+            }
+        }
+ 
+}
