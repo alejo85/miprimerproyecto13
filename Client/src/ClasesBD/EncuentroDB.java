6f814981ddf508c3c31ff49f2dc70d242f3cec82
alejo@masfm.com <alejo@masfm.com@d7aedbdb-e078-e6c9-b6fc-c904269d7ce7>
2012-11-27 21:13:19 +0000

    encuentro de

--- Client/src/ClasesBD/EncuentroDB.java
+++ Client/src/ClasesBD/EncuentroDB.java
@@ -1,6 +1,10 @@
 package ClasesBD;
 
+
 import ClasesLogicas.Encuentro;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class EncuentroDB {
@@ -19,5 +23,22 @@ public class EncuentroDB {
      * @param encuentro
      */
     public void actualizarEncuentro(Encuentro encuentro){
+        }
+    public static int guardarEncuentro(Encuentro encuentro, int idSubRonda) throws SQLException {
+        
+            System.out.println("llego a la base id_encuentro");
+            int idEncuentro=0;
+            Conexion conexion = new Conexion();
+            ResultSet resultado=null;
+            conexion.conectar();
+            String consultasql;
+            
+            consultasql="INSERT INTO encuentro(id_lugar_de_realizacion, id_Subronda )VALUES ('"+encuentro.getLocación().getIdLugar()+"', '"+idSubRonda+"' )RETURNING id_encuentro;";
+            resultado=conexion.consultar(consultasql);
+            resultado.next();
+            System.out.println("despues del next id_encuentro");
+            idEncuentro = resultado.getInt("id_encuentro");
+            return idEncuentro;
+        
         }
 }
